c7bf9b957b3b249c8dfcb1f4a797f9477037d359
tarihay <igor_gorin.02@mail.ru>
2018-09-14 21:27:14 +0600

    new project

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int res = 0;
+        int number = 0;
+        char sign;
+        for (int i = 1; i < args.length; i = i+2) {
+            char[] sim = args[i].toCharArray();
+            number = Integer.valueOf(sim[i]);
+            sign = char.valueOf(sim[i+1]);
+            if (sign == '+') {
+                res = res + number;
+            } else if (sign == '-') {
+                res = res - number;
+            } else if (sign == '*') {
+                res = res * number;
+            } else {
+                res = res / number;
+            }
+            System.out.println(res);
+        }
+    }
+}
+
