03c77b40bec18cf023f5911bd7d92fab928ee9fa
tarihay <igor_gorin.02@mail.ru>
2018-09-26 14:57:23 +0600

    change the word (try 3)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String a = args[1].toUpperCase();
-        String res = args[0].toUpperCase();
-        String res1 = res.replace(a, args[2]);
+        String a = args[1].toUpperCase(); //В строковую переменную a записываю значение 2-го элемента строкового массива args(Заглавная буква)
+        String a1 = args[1].toLowerCase();//В строковую переменную a записываю значение 2-го элемента строкового массива args(Строчная буква)
+        String res = args[0]; //Записываю входную строку, которую мне надо будет изменить
+        String res1 = res.replace(a, args[2]);//Записываю в строковую переменную res1, измененное значение переменной res, в которой изменил заглавные буквы
+        res1 = res1.replace(a1, args[2]);//Обновляю res1, заменяя строчные буквы
 
-
-        System.out.println(res1.toLowerCase());
+        System.out.println(res1);
 
         }
     }
