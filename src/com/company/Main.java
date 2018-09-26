e0a2efa63e299de6603826b1f7779d38346aa71c
tarihay <igor_gorin.02@mail.ru>
2018-09-26 14:26:26 +0600

    change the word

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         String a = args[1].toUpperCase();
-        String res = args[0];
+        String res = args[0].toUpperCase();
         String res1 = res.replace(a, args[2]);
 
-        if (res1 == res){
-            a = args[1].toLowerCase();
-            res1 = res.replace(a, args[2]);
-        }
-        System.out.println(res1);
+
+        System.out.println(res1.toLowerCase());
 
         }
     }
