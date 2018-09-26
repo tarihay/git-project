0738432fdbdb4de063f7a161bdb15df699c840f3
tarihay <igor_gorin.02@mail.ru>
2018-09-26 13:53:21 +0600

    change the word

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int res = 0;
-        int number = 0;
-        char sign;
-        for (int i = 1; i < args.length; i = i+2) {
-            char[] sim = args[i].toCharArray();
-            number = Integer.valueOf(sim[i]);
-            sign = char.valueOf(sim[i+1]);
+        String a = args[1].toUpperCase();
+        String res = args[0];
+        String res1 = res.replace(a, args[2]);
 
-            if (sign == '+') {
-                res = res + number;
-            } else if (sign == '-') {
-                res = res - number;
-            } else if (sign == '*') {
-                res = res * number;
-            } else {
-                res = res / number;
-            }
-            System.out.println(res);
+        if (res1 == res){
+            a = args[1].toLowerCase();
+            res1 = res.replace(a, args[2]);
+        }
+        System.out.println(res1);
+
         }
     }
-}
+
 
